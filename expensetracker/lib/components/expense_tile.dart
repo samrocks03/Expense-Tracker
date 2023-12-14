70d5cc1991f0d8e38d48d1ebaee30363b26bd35e
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-12-14 18:10:06 +0530

    refactor: Improve UI interaction in ListTileWidget
    Refactored the 'ListTileWidget' class to enhance UI interaction by wrapping the ListTile in a Slidable widget.
    
    This allows the addition of sliding actions, such as the ability to delete an expense, making the user interface more intuitive.

--- expensetracker/lib/components/expense_tile.dart
+++ expensetracker/lib/components/expense_tile.dart
@@ -1,24 +1,40 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 class ListTileWidget extends StatelessWidget {
   final DateTime dateTime;
   final String name,amount;
+  void Function(BuildContext)? delTapped;
 
-  const ListTileWidget({
-    super.key, 
+  ListTileWidget({
+    Key? key,
     required this.dateTime,
     required this.name,
-    required this.amount
-  });
+    required this.amount,
+    required this.delTapped, 
+  }) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-    title: Text(name,
-              style:GoogleFonts.gochiHand()),
-    subtitle: Text("${dateTime.day} / ${dateTime.month} / ${dateTime.year}"),
-    trailing: Text("₹ $amount"));
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        children: [
+          SlidableAction(
+            //slidable-button
+            onPressed: delTapped,
+            icon: Icons.delete_sweep_sharp,
+            backgroundColor: Colors.red,)
+        ],
+      ),
+      child: ListTile(
+      title: Text(name,
+                style:GoogleFonts.gochiHand()),
+      subtitle: Text("${dateTime.day} / ${dateTime.month} / ${dateTime.year}"),
+      trailing: Text("₹ $amount")),
+    );
   }
 }
