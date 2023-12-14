9a5a8444f1cd0810f9aca375f308d8320d0b8ae2
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-12-14 14:37:35 +0530

    feat: Initialize Hive in main and open expense_database box
    Updated the main function to asynchronously initialize Hive in the 'main' function.
    
    Opened the 'expense_database' Hive box for data storage.

--- expensetracker/lib/main.dart
+++ expensetracker/lib/main.dart
@@ -4,10 +4,18 @@ import 'package:expensetracker/data/expense_data.dart';
 import 'package:expensetracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+  // initialize HIVE
+  await Hive.initFlutter();
+
+
+  // open a hive-box
+  await Hive.openBox("expense_database");
   runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
