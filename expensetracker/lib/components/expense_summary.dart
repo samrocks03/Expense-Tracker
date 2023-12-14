896c0d2e4ada17c1883be5a9a2c21203a743e2a5
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-12-14 12:18:21 +0530

    feat: Dynamically calculate daily expense summary
    Updated 'ExpenseSummaryWidget' to dynamically calculate daily expense summaries based on the data provided by the 'ExpenseData' provider.
    
    Removed static values and replaced them with dynamic calculations for each day of the week.

--- expensetracker/lib/components/expense_summary.dart
+++ expensetracker/lib/components/expense_summary.dart
@@ -2,6 +2,7 @@
 
 import 'package:expensetracker/bar_graph/bar_graph.dart';
 import 'package:expensetracker/data/expense_data.dart';
+import 'package:expensetracker/datetime/date_time_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,18 +13,30 @@ class ExpenseSummaryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // Get yyyy.mm.dd -> for each day of this week
+    String sunday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 0)));
+    String monday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 1)));
+    String tuesday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 2)));
+    String wednesday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 3)));
+    String thursday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 4)));
+    String friday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 5)));
+    String saturday = convertTheDateTimeToString(startOfWeek.add(Duration(days: 6)));
+
+
     return Consumer<ExpenseData>(
       builder: (context, value, child) => SizedBox(
           height: 200,
           child: MyBarGraph(
               maxY: 100,
-              sundayAmount: 12,
-              mondayAmount: 15,
-              tuesdayAmount: 11,
-              wednesdayAmount: 19,
-              thursdayAmount: 25,
-              fridayAmount: 23,
-              saturdayAmount: 12),
+              sundayAmount: value.calculateDailyExpenseSummary()[sunday] ?? 0,
+              mondayAmount: value.calculateDailyExpenseSummary()[monday] ?? 0,
+              tuesdayAmount: value.calculateDailyExpenseSummary()[tuesday] ?? 0,
+              wednesdayAmount: value.calculateDailyExpenseSummary()[wednesday] ?? 0,
+              thursdayAmount: value.calculateDailyExpenseSummary()[thursday] ?? 0,
+              fridayAmount: value.calculateDailyExpenseSummary()[friday] ?? 0,
+              saturdayAmount: value.calculateDailyExpenseSummary()[saturday] ?? 0
+              ),
         )
     );
   }
