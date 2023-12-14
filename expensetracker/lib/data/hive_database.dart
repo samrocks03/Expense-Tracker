931943f85d00a8d5a50de656aa4b670f9bb209e8
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-12-14 16:39:00 +0530

    feat: Implement readData method in HiveDataBase class
    Added the 'readData' method to the 'HiveDataBase' class, enabling the retrieval of stored expense data from the Hive box.
    
    The method reads the formatted data from the box and converts it back into 'ExpenseItem' objects.

--- expensetracker/lib/data/hive_database.dart
+++ expensetracker/lib/data/hive_database.dart
@@ -4,19 +4,14 @@ import 'package:expensetracker/models/expense_item.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
-
-
-class HiveDataBase{
+class HiveDataBase {
   // reference out box
   final _myBox = Hive.box("expense_database");
 
-
   // write data
-  void saveData(List<ExpenseItem> allExpense){
-    
+  void saveData(List<ExpenseItem> allExpense) {
     // Note - Hive only stores strings and dateTime
-          // not custom objects
-
+    // not custom objects
 
     /**For that reason, we need to convert the ExpenseItem object into those types
           which could be stored in the hive data-formats
@@ -37,8 +32,8 @@ class HiveDataBase{
           --> [name, amount, dateTime]
      */
     List<List<dynamic>> allExpensesFormatted = [];
-  
-    for(var expense in allExpense){
+
+    for (var expense in allExpense) {
       // converting each item into  a list of storable types
       // (string, dateTime)
       List<dynamic> expenseFormatted = [
@@ -52,6 +47,20 @@ class HiveDataBase{
     }
   }
 
-
   // read data
+  List<ExpenseItem> readData() {
+    List savedExpenses = _myBox.get("ALL_EXPENSES") ?? [];
+    List<ExpenseItem> allExpenses = [];
+
+    for (int i = 0; i < savedExpenses.length; i++) {
+      String name = savedExpenses[i][0];
+      String amount = savedExpenses[i][1];
+      DateTime dateTime = savedExpenses[i][2];
+
+      ExpenseItem expense = ExpenseItem(name: name, amount: amount, dateTime: dateTime);
+
+      allExpenses.add(expense);
+    }
+    return allExpenses;
+  }
 }
